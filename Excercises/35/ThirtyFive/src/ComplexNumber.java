1dbf3599ad9f9b2f585e69dc813eec1df25b616b
Krisz <rybarkristik@gmail.com>
2023-09-03 18:31:46 +0200

    compelted some more excercises

---
+++ Excercises/35/ThirtyFive/src/ComplexNumber.java
@@ -0,0 +1,41 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real = this.real + real;
+        this.imaginary = this.imaginary + imaginary;
+
+
+    }
+
+    public void add(ComplexNumber n) {
+        this.real = this.real + n.getReal();
+        this.imaginary = this.imaginary + n.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real = this.real - real;
+        this.imaginary = this.imaginary - imaginary;
+
+    }
+
+    public void subtract(ComplexNumber n) {
+        this.real = this.real - n.getReal();
+        this.imaginary = this.imaginary - n.getImaginary();
+
+    }
+}
