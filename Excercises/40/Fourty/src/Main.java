ec530db9f93dd7016ecbfa8eba563e50b6aa26ca
Krisz <rybarkristik@gmail.com>
2023-09-04 23:29:57 +0200

    more progress

---
+++ Excercises/40/Fourty/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+    }
+}
