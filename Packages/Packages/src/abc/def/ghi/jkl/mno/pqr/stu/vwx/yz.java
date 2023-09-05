b8fd530893248fc85ad66fe77eadd924f8a42902
Krisz <rybarkristik@gmail.com>
2023-09-05 17:11:10 +0200

    Didn't see, but there was another video

---
+++ Packages/Packages/src/abc/def/ghi/jkl/mno/pqr/stu/vwx/yz.java
@@ -0,0 +1,17 @@
+package abc.def.ghi.jkl.mno.pqr.stu.vwx;
+
+public class yz {
+
+        private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public yz(){
+            System.out.println(alphabet);
+        }
+
+    @Override
+    public String toString() {
+        return "yz{" +
+                "alphabet='" + alphabet + '\'' +
+                '}';
+    }
+}
---
+++ Packages/Packages/src/bluu/cat/Main.java
@@ -0,0 +1,12 @@
+package bluu.cat;
+import com.abc.first.Item;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        abc.def.ghi.jkl.mno.pqr.stu.vwx.yz Alphabet = new abc.def.ghi.jkl.mno.pqr.stu.vwx.yz();
+        Item item = new Item("Bluu item");
+        System.out.println(item);
+    }
+}
