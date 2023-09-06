b13932241318a292fcce8a4ba9aa18c4da9daf1a
Krisz <rybarkristik@gmail.com>
2023-09-06 22:09:04 +0200

    more progress in the course

---
+++ List, ArrayList, LinkedList, Iterator, Autoboxing/ArraylList/untitled/src/bluu/cat/Main.java
@@ -0,0 +1,33 @@
+package bluu.cat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(0, new GroceryItem("apples"));
+        System.out.println(groceryList);
+        groceryList.remove(1);
+    }
+}
+
