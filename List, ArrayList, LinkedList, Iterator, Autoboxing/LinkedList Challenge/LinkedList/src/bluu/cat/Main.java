93d489ada96f964c65b754363a06ed81112be6aa
Krisz <rybarkristik@gmail.com>
2023-09-24 21:46:49 +0200

    progressed more in the course

--- List, ArrayList, LinkedList, Iterator, Autoboxing/LinkedList Challenge/LinkedList/src/bluu/cat/Main.java
+++ List, ArrayList, LinkedList, Iterator, Autoboxing/LinkedList Challenge/LinkedList/src/bluu/cat/Main.java
@@ -17,7 +17,6 @@ public class Main {
             (F)orward
             (B)ackward
             (L)ist Places
-            (M)enu
             (Q)uit
             """.strip();
 
@@ -30,44 +29,64 @@ public class Main {
         addPlace(townList, new Place("Perth", 3923));
         addPlace(townList, new Place("Alice Springs", 2771));
         addPlace(townList, new Place("Darwin", 3972));
-        addPlace(townList, new Place("Melbourne", 877));;
-        System.out.println(townList);
+        addPlace(townList, new Place("Melbourne", 877));
+        ;
+        var iterator = townList.listIterator();
+        Scanner sc = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
 
 
-
-
-        while (true) {
+        while (!quitLoop) {
             System.out.println(options);
-            Scanner sc = new Scanner(System.in);
+
+            if (!iterator.hasPrevious()){
+                System.out.println("Originating: " + iterator.next());
+                forward = true;
+            }
+            if (!iterator.hasNext()) {
+                System.out.println("Final: " + iterator.previous());
+                forward = false;
+            }
             String input = sc.nextLine();
 
             switch (input.toUpperCase().charAt(0)) {
-                case 'F' -> System.out.println("Forward");
-                case 'B' -> System.out.println("Backward");
-                case 'L' -> System.out.println("List places");
-                case 'M' -> System.out.println("Menu");
+                case 'F' -> {
+                    if (iterator.hasNext()) {
+                        System.out.println("The next place is: " + iterator.next());
+                    }
+                }
+                case 'B' -> {
+                    if (iterator.hasPrevious()) {
+                        System.out.println("The previous place is: " + iterator.previous());
+                    }
+                }
+                case 'L' -> System.out.println(townList);
                 case 'Q' -> {
                     System.out.println("Quitting...");
                     return;
                 }
-                default -> System.out.println("Unknown input");
+                default -> quitLoop = true;
             }
             System.out.println();
         }
 
 
     }
-    private static void addPlace(LinkedList<Place> list, Place place){
-        if(list.contains(place)){
+
+    private static void addPlace(LinkedList<Place> list, Place place) {
+        if (list.contains(place)) {
             System.out.println("The given place already exists!");
             return;
         }
         int matchedIndex = 0;
-        for(var listPlace: list){
-            if(place.distance() < listPlace.distance()){
+        for (var listPlace : list) {
+            if (place.distance() < listPlace.distance()) {
                 list.add(matchedIndex, place);
-                matchedIndex++;
+                return;
             }
+            matchedIndex++;
         }
+        list.add(place);
     }
 }
