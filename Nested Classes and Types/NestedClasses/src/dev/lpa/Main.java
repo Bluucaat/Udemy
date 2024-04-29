005826a51ac661ab7cc57592e3fc7bd8f44a2f4a
Ribár Krisztián <rybarkristik@gmail.com>
2024-04-29 22:00:56 +0200

    Progressed further in the course, covering inner classes.

--- Nested Classes and Types/NestedClasses/src/dev/lpa/Main.java
+++ Nested Classes and Types/NestedClasses/src/dev/lpa/Main.java
@@ -1,10 +1,10 @@
 package dev.lpa;
 
 import dev.lpa.domain.Employee;
-//import dev.lpa.domain.EmployeeComparator;
+import dev.lpa.domain.StoreEmployee;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -23,7 +23,23 @@ public class Main {
         employees.sort(new Employee.EmployeeComparator<>("yearStarted")
                 .reversed());
 
-        for(Employee e : employees){
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2019, "Walmart"),
+                new StoreEmployee(10105, "Tom", 2020, "Macys"),
+                new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+                new StoreEmployee(10322, "Bud", 2020, "Target")
+        ));
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
         }
     }
