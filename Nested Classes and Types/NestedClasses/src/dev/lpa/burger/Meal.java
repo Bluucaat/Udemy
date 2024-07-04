efbca0157a1f666ed70009a34926ea4d29a6a457
Ribár Krisztián <rybarkristik@gmail.com>
2024-07-04 23:34:06 +0200

    Progressed fruther in the code covering inner classes

--- Nested Classes and Types/NestedClasses/src/dev/lpa/burger/Meal.java
+++ Nested Classes and Types/NestedClasses/src/dev/lpa/burger/Meal.java
@@ -1,9 +1,13 @@
 package dev.lpa.burger;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Meal {
 
-    private final double price = 5.0;
-    private Item burger;
+    private double price = 5.0;
+    private Burger burger;
     private Item drink;
     private Item side;
 
@@ -15,15 +19,16 @@ public class Meal {
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
 
+
     }
 
     public double getTotal(){
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
     @Override
@@ -53,8 +58,59 @@ public class Meal {
                     getPrice(price, conversionRate));
         }
 
+
+
         private static double getPrice(double price, double rate){
             return price * rate;
         }
+    }
+
+    private class Burger extends Item {
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
+
+            private double getPrice(){
+                return switch(this){
+                    case AVOCADO -> 1.0;
+                    case BACON, CHEESE -> 1.5;
+                    default -> 0;
+                };
+            }
+        }
+        private List<Item> toppings = new ArrayList<>();
+        Burger(String name) {
+            super(name, "burger", 5.0);
+        }
+        public double getPrice() {
+            double total = super.price;
+            for(Item topping : toppings){
+                total += topping.price;
+            }
+            return total;
+        }
+
+        public void addToppings(String... selectedToppings){
+            for(String selectedTopping : selectedToppings){
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING",
+                            topping.getPrice()));
+                }catch(IllegalArgumentException ie){
+                    System.out.println("No topping found for " + selectedTopping);
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for (Item topping : toppings){
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
+        }
+    }
+    public void addToppings(String... selectedToppings){
+        burger.addToppings(selectedToppings);
     }
 }
--- Nested Classes and Types/NestedClasses/src/dev/lpa/burger/Store.java
+++ Nested Classes and Types/NestedClasses/src/dev/lpa/burger/Store.java
@@ -3,6 +3,7 @@ package dev.lpa.burger;
 public class Store {
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
+        regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Bacon", "Bacon", "Cheddar");
         System.out.println(regularMeal);
 
         Meal USRegularMeal = new Meal(0.68);
