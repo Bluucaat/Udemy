0e2cdc25e535442676608d2258894d6d414d4505
Ribár Krisztián <rybarkristik@gmail.com>
2024-07-08 14:48:45 +0200

    Completed some more excercises

--- Excercises/section6/src/NumberInWord.java
+++ Excercises/section6/src/NumberInWord.java
@@ -1,3 +1,11 @@
 public class NumberInWord {
-    
+    public static void printNumberInWord(int number){
+    String[] numberWords = new String[]{"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE",
+                                        "SIX", "SEVEN", "EIGHT", "NINE", "TEN"};
+        if(number >= 0 && number < 10) {
+            System.out.println(numberWords[number]);
+        }else{
+            System.out.println("OTHER");
+        }
+    }
 }
