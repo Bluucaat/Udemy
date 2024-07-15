6e9682e2bacaab6702596b854aa19b4c95f830d3
Ribár Krisztián <rybarkristik@gmail.com>
2024-07-15 17:12:39 +0200

    Progressed further, covering Function & Supplier functional interfaces

--- Lambda Expressions/LambdaExpressions/src/Main.java
+++ Lambda Expressions/LambdaExpressions/src/Main.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -62,6 +64,34 @@ public class Main {
         list.removeIf(s -> s.startsWith("ea"));
         list.forEach(s -> System.out.println(s));
 
+        list.replaceAll(s -> s.charAt(0) + " - " + s.toUpperCase());
+        System.out.println("-------");
+        list.forEach(s -> System.out.println(s));
+
+        String[] emptyStrings = new String[10];
+        System.out.println(Arrays.toString(emptyStrings));
+        Arrays.fill(emptyStrings, "");
+        System.out.println(Arrays.toString(emptyStrings));
+
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i+1) + ". ");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i+1) + ". "
+                + switch(i){
+                    case 0 -> "one";
+                    case 1 -> "two";
+                    case 2 -> "three";
+                    default -> "";
+                }
+
+        );
+        System.out.println(Arrays.toString(emptyStrings));
+
+        String[] names = {"Ann", "Bob", "Carol", "David", "Ed", "Fred"};
+        String[] randomList = randomlySelectedValues(15, names, () -> new Random().nextInt(0, names.length));
+        System.out.println(Arrays.toString(randomList));
+
     }
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2){
         T result = function.apply(value1, value2);
@@ -72,4 +102,15 @@ public class Main {
     public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer){
         consumer.accept(t1, t2);
     }
+
+    public static String[] randomlySelectedValues(int count,
+                                                  String[] values,
+                                                  Supplier<Integer> s){
+        String[] selectedValues = new String[count];
+        for(int i = 0; i<count; i++){
+            selectedValues[i] = values[s.get()];
+        }
+        return selectedValues;
+    }
+
 }
