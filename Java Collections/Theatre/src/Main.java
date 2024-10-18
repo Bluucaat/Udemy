85136af9c06c6eb3cfb6e3484997f6d6a94bee83
Ribar Krisztian <rybarkristik@gmail.com>
2024-10-18 14:56:12 +0900

    covered TitledPane/Accordion controls, JavaFX

--- Java Collections/Theatre/src/Main.java
+++ Java Collections/Theatre/src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("The great theatre", 10, 100);
         theatre.printTheatreMap();
-
         }
 }
